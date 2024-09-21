1e66e80ec1a0ab1bfd07f7864982c010f2e867e6
Karl <karl.mullr@gmail.com>
2024-09-21 22:31:09 +0200

    add haslike in controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Mimachh\LikeIt\Http\Controllers\LikeController;
 Route::post('/{type}/{id}/like', [LikeController::class, 'like'])->name('like');
 Route::delete('/{type}/{id}/unlike', [LikeController::class, 'unlike'])->name('unlike');
 Route::post('/{type}/{id}/toggle-like', [LikeController::class, 'toggleLike'])->name('toggle-like');
+Route::get('/{type}/{id}/has-like', [LikeController::class, 'hasLike']);
