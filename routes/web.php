6615a4417e9fb5c2cf48034141677724628aa841
Karl <karl.mullr@gmail.com>
2024-09-21 19:43:26 +0200

    feat: :sparkles: add the toggle like route

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Mimachh\LikeIt\Http\Controllers\LikeController;
 
 Route::post('/{type}/{id}/like', [LikeController::class, 'like'])->name('like');
 Route::delete('/{type}/{id}/unlike', [LikeController::class, 'unlike'])->name('unlike');
+Route::post('/{type}/{id}/toggle-like', [LikeController::class, 'toggleLike'])->name('toggle-like');
